8cf49df37fb8bb2f1650d9279078ddf70159da58
Anderson Leal <andersonofl@gmail.com>
2024-02-17 15:15:05 -0300

    Resposta

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-    </BrowserRouter>,
+    </BrowserRouter>
   </React.StrictMode>
 )
 
